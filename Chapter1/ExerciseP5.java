1416caa14eb4761b710ec79508ed67816d90ed1d
James Rosado <jarosado0911@gmail.com>
2021-03-10 20:21:48 -0500

    added more simple java programs

---
+++ Chapter1/ExerciseP5.java
@@ -0,0 +1,19 @@
+/*
+This program computes the sum of the first ten positive integers.
+*/
+public class ExerciseP5 {
+    
+    public static void main(String[] args)
+    {
+        System.out.println(1);
+        System.out.println(1+2);
+        System.out.println(1+2+3);
+        System.out.println(1+2+3+4);
+        System.out.println(1+2+3+4+5);
+        System.out.println(1+2+3+4+5+6);
+        System.out.println(1+2+3+4+5+6+7);
+        System.out.println(1+2+3+4+5+6+7+8);
+        System.out.println(1+2+3+4+5+6+7+8+9);
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+    }
+}
---
+++ Chapter1/ExerciseP6.java
@@ -0,0 +1,19 @@
+/*
+This program prints the sum of the first 10 reciprocals of integers
+*/
+public class ExerciseP6 {
+    
+    public static void main(String[] args)
+    {
+        System.out.println(1.0/1.0);
+        System.out.println(1.0/1.0+1.0/2.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0+1.0/4.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0+1.0/4.0+1.0/5.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0+1.0/4.0+1.0/5.0+1.0/6.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0+1.0/4.0+1.0/5.0+1.0/6.0+1.0/7.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0+1.0/4.0+1.0/5.0+1.0/6.0+1.0/7.0+1.0/8.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0+1.0/4.0+1.0/5.0+1.0/6.0+1.0/7.0+1.0/8.0+1.0/9.0);
+        System.out.println(1.0/1.0+1.0/2.0+1.0/3.0+1.0/4.0+1.0/5.0+1.0/6.0+1.0/7.0+1.0/8.0+1.0/9.0+1.0/10.0);
+    }
+}
