1e26010c4f0864d5dce55d211b47571d767201c8
James Rosado <jarosado0911@gmail.com>
2021-03-10 19:59:55 -0500

    initial commit

---
+++ Chapter1/ExerciseP1.java
@@ -0,0 +1,12 @@
+/*
+This program displays my middle name inside a box on the screen
+*/
+public class ExerciseP1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("+-------+");
+        System.out.println("|Michael|");
+        System.out.println("+-------+");
+    }
+}
